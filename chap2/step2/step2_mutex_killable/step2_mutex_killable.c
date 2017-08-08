3a5efd508b264e72ecbbb451b994b3c7883b3b6f
hal0taso <yuya7719guitar@gmail.com>
2017-08-08 21:34:19 +0900

    [FIX] step2_mutex_killable.c: エラーハンドリングの処理を追記。 SIGKILLを送信したときにエラーメ ッセージの表示がされることを確認。

--- chap2/step2/step2_mutex_killable/step2_mutex_killable.c
+++ chap2/step2/step2_mutex_killable/step2_mutex_killable.c
@@ -10,19 +10,27 @@ int step2_init(void)
 {
   printk(KERN_ALERT "step2 LOADED.\n");
 
+  printk("first lock is LOCKING...\n");
   if(mutex_lock_killable(&lock)) {
+    printk("While first lock, interrupted.\n");
     return -EINTR;
   }
-  printk(KERN_ALERT "mutex is LOCKED.\n");
 
-  printk(KERN_ALERT "Try to lock mutex again.\n");
+  
+  printk("second lock is LOCKING...\n");  
+  printk(KERN_INFO "Try to lock mutex again.\n");
   if (mutex_lock_killable(&lock)) {
+    printk("While second lock, interrupted.\n");
+    mutex_unlock(&lock);
     return -EINTR;
   }
 
+  
+  printk(KERN_INFO "lock is UNLOCKED.\n");
   mutex_unlock(&lock);
+  printk(KERN_INFO "lock is UNLOCKED.\n");
   mutex_unlock(&lock);
-
+  
   return 0;
 }
 
