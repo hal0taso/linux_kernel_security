f98a6cc0a5e92794866cc024135358c1b37cd94f
hal0taso <yuya7719guitar@gmail.com>
2017-08-08 13:47:43 +0900

    [FIX] step21.c

--- chap2/step21/step21.c
+++ chap2/step21/step21.c
@@ -1,20 +1,18 @@
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <time.h>
+#include <linux/init.h>
 
 static int step21_init(void)
 {
   while(1){
-    sleep(1);
     printk(KERN_ALERT "Running...\n");
   }
   return 0;
 }
 
-static int step21_exit(void)
+static void step21_exit(void)
 {
   printk(KERN_ALERT "step22 unloaded.\n");
 }
 
 module_init(step21_init);
-modlue_exit(step21_exit);
+module_exit(step21_exit);
