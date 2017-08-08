4c5922df2bc903c3a5b2e045a53eaf284aa253a0
hal0taso <yuya7719guitar@gmail.com>
2017-08-09 08:21:09 +0900

    [FIX] chap/step6/step6.c バグは殺す

--- chap2/step6/step6.c
+++ chap2/step6/step6.c
@@ -1,14 +1,16 @@
 /*
-
+  allocating memory dinamicaly by using kmalloc.
 */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL v2");
 
 void mset(char *buf, size_t size)
 {
+  int i;
   for(i = 0; i < size; i ++){
     *buf = (char) 0;
     buf += sizeof(char);
@@ -19,7 +21,10 @@ int step6_init(void)
 {
   int size = 4096;
   int i;
+  char *ptr;
+  
   printk(KERN_ALERT "step6 LOAD\n");
+
   for(i = 10; i < 0; i --){
     while(1){
       ptr = kmalloc(size, GFP_ATOMIC);
@@ -27,7 +32,7 @@ int step6_init(void)
         printk("break\n");
         break;
       }
-      mset(ptr);
+      mset(ptr, size);
       printk("[%p] kmalloc allocated.\n", ptr);
     }
     size /= 2;
@@ -36,7 +41,7 @@ int step6_init(void)
   return 0;
 }
 
-int step6_exit(void)
+void step6_exit(void)
 {
   printk(KERN_ALERT "step6 UNLOADED\n");
 }
